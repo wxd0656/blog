07f6c0a93d7231e20b977f763590684f8d452161
wxd0656 <wxd0656@163.com>
2016-04-06 09:40:15 +0800

    用户模型的数据库操作

---
+++ proxy/user.js
@@ -0,0 +1,15 @@
+var User = require('../models/user.js');
+
+/**
+ * 根据登录名查找用户
+ * Callback:
+ * - err, 数据库异常
+ * - user, 用户
+ * @param {String} loginName 登录名
+ * @param {Function} callback 回调函数
+ */
+exports.getUserByLoginName = function(loginName, callback) {
+	User.findOne({'loginname': loginName}, callback)
+}
+
+
