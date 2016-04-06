befe8b9720bf2c62f6e07ab53bcb9037d687253a
wxd0656 <wxd0656@163.com>
2016-04-06 17:58:23 +0800

    添加渲染路由

--- router.js
+++ router.js
@@ -1,11 +1,15 @@
 var express = require('express');
 
 var site = require('./controllers/site.js');
+var sign = require('./controllers/sign.js');
 
 var router = express.Router();
 
 // home page
 router.get('/', site.index);
+
+// login page
+router.get('/login', sign.renderLogin);
 
 
 module.exports = router;
