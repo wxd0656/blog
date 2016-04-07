46b401f864a61ef4a03ec5c5de414cc231eb0b35
wxd0656 <wxd0656@163.com>
2016-04-07 14:25:38 +0800

    新建文章模型

---
+++ models/article.js
@@ -0,0 +1,41 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var _ = require('lodash');
+var config = require('../config.js');
+
+
+var ArticleSchema = new Schema({
+	title: { type:String },
+	content: { type:String },
+	author_id: { type:ObjectId },
+	top: { type:boolean, default:false },
+	tag: { type:String },
+	type: { type:String }, //文章类型
+	visity_count: { type:Number },
+	reply_count: { type:Number },
+
+	create_at: { type:Date, default:Date.now },
+	update_at: { type:Date, default:Date.now },
+	last_reply: { type:ObjectId },
+	last_reply_at: { type:Date, default:Date.now },
+
+	deleted: { type:boolean, default:false }
+});
+
+
+ArticleSchema.index({create_at: -1});
+ArticleSchema.index({tag: 1});
+ArticleSchema.index({type: 1});
+
+ArticleSchema.virtual('article_type').get(function () {
+	var type = this.type;
+	var pair = _.find(config.article_type, function(_type) {
+		return type === _type[0];
+	});
+
+	if (pair) {
+		return pair[1];
+	} else {
+		return '';
+	}
+});
