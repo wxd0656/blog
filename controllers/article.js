e3da882e1d70e59bea4f99b1dd54114f6053a58a
wxd0656 <wxd0656@163.com>
2016-04-09 21:06:18 +0800

    初步增加新建文章的功能

---
+++ controllers/article.js
@@ -0,0 +1,52 @@
+
+var config = require('../config.js');
+var logger = require('../common/logger.js');
+var Article = require('../proxy/article.js');
+
+/**
+ * render the page of CreateNewArticle
+ * @param  {[type]}   req  
+ * @param  {[type]}   res  
+ * @param  {Function} next 
+ */
+exports.renderCreateNewArticle = function(req, res, next) {
+	res.render('article/edit', {types:config.article_type});
+}
+
+
+
+exports.createNewArticle = function(req, res, next) {
+	var title = req.body.title;
+	var content = req.body.content;
+	var articleType = req.body.article_type;
+
+	var types = config.article_type.map(function(item) {
+		return item[0];
+	});
+
+	var editError = '';
+	if (!title) {
+		editError = '标题不能是空的。';
+	} else if (!content) {
+		editError = '内容不可为空';
+	} else if (!articleType || types.indexOf(articleType) == -1) {
+		editError = '内容不可为空';
+	}
+
+	if (editError) {
+		return res.render('article/edit', {
+			edit_error: editError,
+			title: title,
+			content: content,
+			types: config.article_type
+		});
+	}
+
+	Article.saveNewArticle(title, content, articleType, 'testid123123', function(err, article){
+		if (err) {
+			logger.dblogger.error('save new article error : ' + err.message);
+			return next(err);
+		}
+		res.redirect('');
+	});
+}
