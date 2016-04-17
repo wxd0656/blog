9394a721cd3871e02d40b1d9eabe3f47328ca150
wxd <wxd0656@163.com>
2016-04-17 19:59:15 +0800

    添加登录验证路由

--- router.js
+++ router.js
@@ -3,6 +3,7 @@ var express = require('express');
 var site = require('./controllers/site.js');
 var sign = require('./controllers/sign.js');
 var article = require('./controllers/article.js');
+var auth = require('./mid/auth.js');
 
 var router = express.Router();
 
@@ -13,13 +14,13 @@ router.get('/', site.index);
 router.get('/login', sign.renderLogin);
 router.post('/login', sign.handerLogin);
 
-router.get('/article/create', article.renderCreateNewArticle);
-router.post('/article/create', article.createNewArticle);
+router.get('/article/create', auth.loginRequire, article.renderCreateNewArticle);
+router.post('/article/create', auth.loginRequire, article.createNewArticle);
 router.get('/article/:aid', article.articleDetial);
-router.post('/article/:aid/delete', article.deleteArticle);
-router.post('/article/:aid/top', article.topArticle);
-router.get('/article/:aid/edit', article.renderEdit);
-router.post('/article/:aid/edit', article.editArticle);
+router.post('/article/:aid/delete', auth.loginRequire, article.deleteArticle);
+router.post('/article/:aid/top', auth.loginRequire, article.topArticle);
+router.get('/article/:aid/edit', auth.loginRequire, article.renderEdit);
+router.post('/article/:aid/edit', auth.loginRequire, article.editArticle);
 
 
 
